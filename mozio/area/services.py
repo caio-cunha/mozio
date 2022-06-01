b82af3931ae48dc834c22ed7a46f658696e2356e
caio-cunha <caiocomputacao2014@gmail.com>
2022-06-01 18:57:06 -0300

    create endpoint for area

--- mozio/area/services.py
+++ mozio/area/services.py
@@ -16,3 +16,19 @@ class AreaService():
         areas = Area.objects.all()
 
         return areas
+
+    def create(self, serializer):
+        """
+        A service for create area (polygon).
+
+        Args:
+
+            serializer - serializer validated for save.
+
+        Response:
+
+            data - serializer data.
+        """
+        serializer.save()
+
+        return serializer.data
--- mozio/area/views.py
+++ mozio/area/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from rest_framework import status
 from area.services import AreaService
 from area.serializers import AreaSerializer
 
@@ -22,6 +24,23 @@ class AreaView(APIView, LimitOffsetPagination):
         results = self.paginate_queryset(areas, request, view=self)
         serializer = AreaSerializer(results, many=True)
         return self.get_paginated_response(serializer.data)
+
+    def post(self, request):
+        """
+        A view for create area (polygon).
+
+        Args:
+            request - Area request object.
+
+        Returns:
+            response - Provider created.
+        """
+        area_service = AreaService()
+        serializer = AreaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        area = area_service.create(serializer=serializer)
+        return Response(area, status=status.HTTP_201_CREATED)
+
 
 class AreaViewDetail(APIView):
     pass
