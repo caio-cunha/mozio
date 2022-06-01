c07064c72e6783676f5f2ce335d32c91c4310485
caio-cunha <caiocomputacao2014@gmail.com>
2022-06-01 10:09:28 -0300

    model and settings for provider

--- mozio/provider/models.py
+++ mozio/provider/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+class Provider(TimeStampedModel):
+
+    name = models.CharField(null=False, blank=False, max_length=100)
+    email = models.EmailField(null=False, blank=False, max_length=100)
+    phone = models.CharField(null=True, blank=True, max_length=50)
+    language = models.CharField(null=True, blank=True, max_length=50)
+    currency = models.CharField(null=True, blank=True, max_length=50)
+
+
