03692d23d2e5ac676b9681ce3b56bf1338bcd3e8
caio-cunha <caiocomputacao2014@gmail.com>
2022-06-02 10:25:11 -0300

    change some fields in models

--- mozio/provider/models.py
+++ mozio/provider/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 
+LANGUAGE_CHOICES = [
+    ('BRA', 'Brazil'),
+    ('SPA', 'Spanish'),
+    ('ENG', 'English'),
+    ('CHI', 'Chinese'),
+    ('AR', 'Arabic'),
+    ('HIN', 'Hindi'),
+    ('JPN', 'Japanese'),
+    ('GER', 'German'),
+    ('FRA', 'French'),
+]
+
+CURRENCY_CHOICES = [
+    ('BRL', 'Real'),
+    ('EUR', 'Euro'),
+    ('JPY', 'Japanese Yen'),
+    ('CNY', 'Chinese Yuan'),
+    ('INR', 'Indian Rupee'),
+    ('USD', 'US Dollar'),
+    ('GBP', 'Pound'),
+]
+
 class Provider(TimeStampedModel):
 
     name = models.CharField(null=False, blank=False, max_length=100)
     email = models.EmailField(null=False, blank=False, max_length=100)
     phone = models.CharField(null=True, blank=True, max_length=50)
-    language = models.CharField(null=True, blank=True, max_length=50)
-    currency = models.CharField(null=True, blank=True, max_length=50)
+    language = models.CharField(null=True, blank=True, max_length=3, choices=LANGUAGE_CHOICES)
+    currency = models.CharField(null=True, blank=True, max_length=3, choices=CURRENCY_CHOICES)
 
 
